0c7fb675595a066478d74f492789e39064c90871
MacTavish Lee <1064416560@qq.com>
2019-06-07 10:58:09 +0800

    :sparkles: feat: 完成字符串翻转算法

---
+++ Algorithm/Test/string.test.js
@@ -0,0 +1,5 @@
+import revertByWord from '../Code/string.js'
+
+test('revertByWord: Let\'s take LeetCode contest', () => {
+  expect(revertByWord("Let's take LeetCode contest")).toBe("s'teL ekat edoCteeL tsetnoc");
+})
