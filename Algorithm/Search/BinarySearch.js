0b498c7bbbe7b57709c70b5feb2149c85d8db0ba
Reaper622 <1064416560@qq.com>
2019-10-31 09:41:40 +0800

    :sparkles: feat: 添加二分算法

---
+++ Algorithm/Search/BinarySearch.js
@@ -0,0 +1,30 @@
+"use strict";
+exports.__esModule = true;
+/**
+ * 给定一个 n 个元素有序的（升序）整型数组 nums 和一个目标值 target
+ * 写一个函数搜索 nums 中的 target，如果目标值存在返回下标，否则返回 -1。
+ * @param {number[]} nums 要查询的数组
+ * @param {number} target 查找的目标值
+ */
+function BinarySearch(nums, target) {
+    // 定义左索引和右索引和中间指示符
+    var low = 0;
+    var high = nums.length - 1;
+    var mid = 0;
+    // 循环查找
+    while (low <= high) {
+        mid = Math.floor((low + high) / 2);
+        if (target === nums[mid]) {
+            return mid;
+        }
+        else if (target > nums[mid]) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+    // 若找不到 则返回-1
+    return -1;
+}
+exports.BinarySearch = BinarySearch;
---
+++ Algorithm/Test/Search.test.js
@@ -0,0 +1,5 @@
+import {BinarySearch} from '../Search/BinarySearch'
+
+test('BinarySearch', () => {
+    expect(BinarySearch([-1,0,3,5,9,12], 9)).toBe(4)
+})
