1755ff8d026227a03f47ab011651850ec0edc1bc
Reaper622 <1064416560@qq.com>
2019-11-01 19:31:29 +0800

    :zap: feat: 添加获取旋转数组中元素位置的测试

--- Algorithm/Test/Search.test.js
+++ Algorithm/Test/Search.test.js
@@ -1,5 +1,6 @@
 import {BinarySearch} from '../Search/BinarySearch'
 import {Sqrt} from '../Search/Sqrt'
+import {GetIndexFromRotateArray} from '../Search/GetIndexFromRotateArray'
 import {FindPeakElement} from '../Search/FindPeakElement'
 
 test('BinarySearch', () => {
@@ -9,6 +10,10 @@ test('BinarySearch', () => {
 test('Sqrt', () => {
     expect(Sqrt(9)).toBe(3)
     expect(Sqrt(16)).toBe(4)
+})
+
+test('GetIndexFromRotateArray', () => {
+    expect(GetIndexFromRotateArray([4,5,6,7,0,1,2], 1)).toBe(5)
 })
 
 test('FindPeak', () => {
